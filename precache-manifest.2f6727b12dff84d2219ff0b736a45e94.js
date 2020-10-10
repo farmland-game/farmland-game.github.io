9ce7c5bdd270c4bacf775b845aafd9af3a5117c8
spartacrypt <spartacrypt@users.noreply.github.com>
2020-10-10 17:18:55 +0000

    deploy: farmland-game/farmland@28f013b98379394faa985bfff089f3e4e48f026f

--- precache-manifest.2f6727b12dff84d2219ff0b736a45e94.js
+++ precache-manifest.2f6727b12dff84d2219ff0b736a45e94.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "13d4b23bf7b41fae605d37137cc4ce3e",
+    "revision": "6a355e43da40a319c05b542d0f592d8e",
     "url": "/index.html"
   },
   {
-    "revision": "dbfb4d8759c2a940297d",
-    "url": "/static/css/main.f33175a6.chunk.css"
+    "revision": "0bdf092c15a4ad72c236",
+    "url": "/static/css/main.94d40a99.chunk.css"
   },
   {
     "revision": "a7c2d7a1c226ee094aaa",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6c22963a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dbfb4d8759c2a940297d",
-    "url": "/static/js/main.f5e485ee.chunk.js"
+    "revision": "0bdf092c15a4ad72c236",
+    "url": "/static/js/main.5ce61d6b.chunk.js"
   },
   {
     "revision": "3c155fd13db85d30fcf5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a50159f0a5121ce785ff1a82037ee8f.js"
+  "/precache-manifest.2f6727b12dff84d2219ff0b736a45e94.js"
 );
 
 self.addEventListener('message', (event) => {
